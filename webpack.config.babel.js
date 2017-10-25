107568c057771bda308e3398556e0ca5005abd2b
Maxwell Lasky <mlasky46@gmail.com>
2017-10-25 10:33:12 -0600

    some small enhancements

---
+++ webpack.config.babel.js
@@ -0,0 +1,67 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CleanPlugin from 'clean-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'motili-widget.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader?limit=10000'
+      },
+      {
+        test: /\.woff|\.woff2|\.svg|\.eot|\.ttf/,
+        loader: 'url-loader?prefix=font/&limit=10000'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new CleanPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: false,
+      filename: 'index.html'
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: 'assets',
+        to: 'assets',
+        context: `${__dirname}/src`
+      }
+    ]),
+    // new UglifyJSPlugin({ uglifyOptions: {
+    //   compress: true,
+    //   minimize: true
+    // }}),
+    // new webpack.DefinePlugin({WP_ENVIRONMENT: JSON.stringify(process.env.NODE_ENV)}),
+  ]
+};
